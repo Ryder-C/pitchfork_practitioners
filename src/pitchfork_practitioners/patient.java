19def559af20f92ea84d9d6b8d379fb4a6c8090b
leonmarchand <leonmarchand@localhost>
2024-03-19 21:50:49 -0700

    structure and scene builder, compiling

--- src/pitchfork_practitioners/patient.java
+++ src/pitchfork_practitioners/patient.java
@@ -3,7 +3,6 @@ package pitchfork_practitioners;
 
 public class patient extends user{
 	private String phoneNumber;
-    private String preferredPharmacy;
     private String emailAddress;
     private String homeAddress;
     
