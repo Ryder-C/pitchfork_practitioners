b6ea12f044229257db49ec9536d612eafc1f2190
Jesus Cruz <127355824+jcmb-11@users.noreply.github.com>
2024-04-08 20:21:21 -0700

    Update to Patient.java
    added attributes for pharmacy, insurance, vaccines, previous conditions, and previous medications. Also added getters and setters for all

--- src/pitchfork_practitioners/Patient.java
+++ src/pitchfork_practitioners/Patient.java
@@ -10,7 +10,12 @@ public class Patient {
 	private String patientID;
 	private String phoneNumber;
     private String emailAddress;
-    private String homeAddress;   
+    private String homeAddress; 
+    private String prefferedPharmacy;  
+    private String insuranceInfo;  
+    private String vaccines; 
+    private String prevConditions; 
+    private String prevMeds;  
     
 	public Patient(String patientID, String phoneNumber, String emailAddress, String homeAddress) {
 		this.patientID = patientID;
@@ -18,8 +23,16 @@ public class Patient {
 		this.emailAddress = emailAddress;
 		this.homeAddress = homeAddress;
 	}
-    
-    // Patient save method
+	
+	
+    //default constructor
+    public Patient() {
+		
+	}
+
+
+
+	// Patient save method
     // Please change this method if you change the types of data stored in the patient file
 	public void saveToFile(File patientRecord) throws IOException {
 		FileWriter writer = new FileWriter(patientRecord);
@@ -27,6 +40,12 @@ public class Patient {
 		Database.saveValue(writer, "Phone Number", phoneNumber);
 		Database.saveValue(writer, "Email Address", emailAddress);
 		Database.saveValue(writer, "Home Address", homeAddress);
+		Database.saveValue(writer, "Preferred Pharmacy", prefferedPharmacy);
+		Database.saveValue(writer, "Insurance Information", insuranceInfo);
+		Database.saveValue(writer, "Vaccines", vaccines);
+		Database.saveValue(writer, "Previous Conditions", prevConditions);
+		Database.saveValue(writer, "Previous Medications", prevMeds);
+		
 		writer.close();
 	}
     
@@ -42,5 +61,77 @@ public class Patient {
 		
 		
 		return new Patient(patientID, phoneNumber, emailAddress, homeAddress);
+	}
+
+	public String getPatientID() {
+		return patientID;
+	}
+
+	public void setPatientID(String patientID) {
+		this.patientID = patientID;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getHomeAddress() {
+		return homeAddress;
+	}
+
+	public void setHomeAddress(String homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+
+	public String getPrefferedPharmacy() {
+		return prefferedPharmacy;
+	}
+
+	public void setPrefferedPharmacy(String prefferedPharmacy) {
+		this.prefferedPharmacy = prefferedPharmacy;
+	}
+
+	public String getInsuranceInfo() {
+		return insuranceInfo;
+	}
+
+	public void setInsuranceInfo(String insuranceInfo) {
+		this.insuranceInfo = insuranceInfo;
+	}
+
+	public String getVaccines() {
+		return vaccines;
+	}
+
+	public void setVaccines(String vaccines) {
+		this.vaccines = vaccines;
+	}
+
+	public String getPrevConditions() {
+		return prevConditions;
+	}
+
+	public void setPrevConditions(String prevConditions) {
+		this.prevConditions = prevConditions;
+	}
+
+	public String getPrevMeds() {
+		return prevMeds;
+	}
+
+	public void setPrevMeds(String prevMeds) {
+		this.prevMeds = prevMeds;
 	}
 }
