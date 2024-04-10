000e51e8c7ef22009398406b5fb3c7b55ce71fa2
Ryder <rydercasazza@gmail.com>
2024-04-10 14:21:09 -0700

    Added simple message saving, loading, and a new Message class.

---
+++ src/pitchfork_practitioners/Message.java
@@ -0,0 +1,56 @@
+package pitchfork_practitioners;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Message {
+	private String text;
+	private String sender_id;
+	private String receiver_id;
+	
+	public Message(String text, String sender_id, String receiver_id) {
+		this.text = text;
+		this.sender_id = sender_id;
+		this.receiver_id = receiver_id;
+	}
+	
+	public void saveMessage(File messageRecord) throws IOException {
+		FileWriter writer = new FileWriter(messageRecord);
+		Database.saveValue(writer, sender_id, text);
+		writer.close();
+	}
+	
+	public static Message[] loadAllMessages(File messageRecord, String viewerID, String otherID) throws IOException {
+		if (!messageRecord.isFile()) {
+			throw new IOException();
+		}
+		
+		Scanner scanner = new Scanner(messageRecord);
+		ArrayList<Message> messages = new ArrayList<Message>();
+		while (scanner.hasNextLine()) {
+			String text = scanner.nextLine();
+			String[] temp = text.split(": ");
+			String id = temp[0].strip();
+			String data = temp[1].strip();
+			
+			if (id.equals(viewerID)) {
+				messages.add(new Message(data, viewerID, otherID));
+			} else {
+				messages.add(new Message(data, otherID, viewerID));
+			}
+		}
+		scanner.close();
+		return messages.toArray(new Message[messages.size()]);
+	}
+	
+	public String getSenderID() {
+        return sender_id;
+    }
+	
+	public String getReceiverID() {
+        return receiver_id;
+    }
+}
